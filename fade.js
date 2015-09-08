92be3a4c4f62fb2d4140d550c8217d6a7abaf265
terrywen412 <MacbookPro@Terrys-MacBook-Pro.local>
2015-09-07 22:29:11 -0700

    major update

---
+++ fade.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#fade").css("display", "none");
+    $("#fade").fadeIn(1000);
+    $("#buttonFade").fadeIn(3000);
+/*    $("a.transition").click(function(event){
+        event.preventDefault();
+		linkLocation = this.href;
+		$("body").fadeOut(1000, redirectPage);		
+	});
+		
+	function redirectPage() {
+		window.location = linkLocation;
+	}*/
+});
